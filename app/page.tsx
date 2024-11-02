e5e59730af38796650a184faa512dca2f64f4273
DiegoDev <dmilanop28@gmail.com>
2024-11-01 22:53:05 -0400

    :art: style: add the StorePorivider and button to the counter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+import StoreProvider from "./StoreProvider";
+
 export default function App() {
   return (
-    <div className='flex items-center justify-center h-svh'>
-      <h1 className='text-4xl font-bold' >TEMPLATE WITH CLEAN ARCHITECTURE</h1>
-    </div>
+    <StoreProvider>
+      <div className='flex flex-col items-center justify-center h-svh gap-y-5'>
+        <h1 className='text-4xl font-bold' >TEMPLATE WITH CLEAN ARCHITECTURE</h1>
+        
+        <Link href={'/counter'}>
+          <button
+            className='text-base font-semibold border rounded border-blue-700 w-40 h-10 hover:bg-blue-700 hover:text-white'
+          >
+            IR AL COUNTER
+          </button>
+        </Link>
+      </div>
+    </StoreProvider>
   );
 };
