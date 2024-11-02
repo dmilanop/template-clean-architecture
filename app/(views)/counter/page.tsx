9272e76675556e61c8f17d19cc1e2489ee58df9e
DiegoDev <dmilanop28@gmail.com>
2024-11-01 23:02:26 -0400

    :art: style: add counter page

---
+++ app/(views)/counter/page.tsx
@@ -0,0 +1,7 @@
+export default function Counter () {
+  return (
+    <div className='flex items-center justify-center h-svh'>
+      <h1 className='text-4xl font-bold' >Counter Page</h1>
+    </div>
+  )
+}
