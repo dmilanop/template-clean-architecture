9b2fcfa1d1e5a376da0ee5e7fb77fdb60cdbb8fe
DiegoDev <dmilanop28@gmail.com>
2024-11-04 10:50:32 -0400

    :art: style: add home button

--- app/(views)/counter/page.tsx
+++ app/(views)/counter/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Counter () {
   return (
-    <div className='flex items-center justify-center h-svh'>
-      <h1 className='text-4xl font-bold' >Counter Page</h1>
+    <div className='flex flex-col items-center justify-center h-svh gap-y-5'>
+      <h1 className='text-4xl font-bold' >COUNTER PAGE</h1>
+      
+      <Link href={'/'} >
+        <button
+          className='text-base font-semibold border rounded border-blue-700 w-40 h-10 hover:bg-blue-700 hover:text-white'
+        >
+          IR AL HOME
+        </button>
+      </Link>
     </div>
   )
 }
