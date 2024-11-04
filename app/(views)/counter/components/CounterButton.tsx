e65061419b648eb9130bde4188838360c248aeb1
DiegoDev <dmilanop28@gmail.com>
2024-11-04 13:00:45 -0400

    :art: :wrench: style & feat: add style to the component and button to increment the counter

--- app/(views)/counter/components/CounterButton.tsx
+++ app/(views)/counter/components/CounterButton.tsx
@@ -1,5 +1,22 @@
+'use client'
+import { increment } from '@/lib/features/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { RootState } from '@/lib/store';
+
 export const CounterButton = () => {
+  const counter  = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <span>Yo soy componente CounterButton</span>
+    <div className='flex flex-col items-center'>
+      <button
+        className='text-base font-semibold mt-14 border rounded border-blue-700 w-40 h-10 hover:bg-blue-700 hover:text-white'
+        onClick={() => dispatch(increment(1))}
+      >
+        Increment + 1 
+      </button>
+
+      <span className='text-xl font-bold mt-7' >Current value: {counter}</span>
+    </div>
   );
 };
--- app/(views)/counter/page.tsx
+++ app/(views)/counter/page.tsx
@@ -5,8 +5,8 @@ export default function Counter () {
   return (
     <div className='flex flex-col items-center justify-center h-svh gap-y-5'>
       <h1 className='text-4xl font-bold' >COUNTER PAGE</h1>
-      
-      <div className='flex items-center gap-x-5'>
+
+      <div className='flex justify-center items-center gap-x-5'>
         <Link href={'/'} >
           <button
             className='text-base font-semibold border rounded border-blue-700 w-40 h-10 hover:bg-blue-700 hover:text-white'
